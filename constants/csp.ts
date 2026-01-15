8dbab0d55e8f256c3a1351d3f6b0d405cfca6170
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2026-01-15 07:40:18 -0500

    update csp

--- constants/csp.ts
+++ constants/csp.ts
@@ -1,6 +1,9 @@
-const ZITADEL_DOMAIN = process.env.ZITADEL_API_URL
-  ? new URL(process.env.ZITADEL_API_URL).hostname
-  : '*.zitadel.cloud';
-
-export const DEFAULT_CSP =
-  `default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; connect-src 'self'; child-src; style-src 'self' 'unsafe-inline'; font-src 'self'; object-src 'none'; img-src 'self' https://${ZITADEL_DOMAIN};`;
+export const DEFAULT_CSP = `default-src 'self';
+   script-src 'self' 'unsafe-inline' 'unsafe-eval';
+   style-src 'self' 'unsafe-inline';
+   img-src 'self' blob: data;
+   font-src 'self';
+   object-src 'none';
+   base-uri 'self';
+   connect-src 'self';
+   `;
