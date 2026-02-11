1a6084e41fa4993146b2ab092d6634c19fea13b1
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2026-02-11 11:44:30 -0500

    Fix type check

--- app/(api)/logout-session/route.ts
+++ app/(api)/logout-session/route.ts
@@ -4,11 +4,9 @@ import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const organization = searchParams.get("organization") || undefined;
   const returnUrl = searchParams.get("returnUrl") || "/start";
 
   await logoutCurrentSession({
-    organization,
     postLogoutRedirectUri: returnUrl,
   });
 
