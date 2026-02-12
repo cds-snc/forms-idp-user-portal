a9308ce19f4c1fea554ce89871764e0ee31d8e70
Peter Thiessen <peter.thiessen@cds-snc.ca>
2026-02-12 15:42:40 -0500

    add authentication component with placeholder content

--- app/account/components/AccountInformation.tsx
+++ app/account/components/AccountInformation.tsx
@@ -1,6 +1,7 @@
 import { LinkButton } from "@components/serverComponents/globals/Buttons/LinkButton";
 
-// TODO move to own route and file directory location later
+// TODO add translation strings
+
 export const AccountInformation = ({
   firstName,
   lastName,
@@ -14,7 +15,7 @@ export const AccountInformation = ({
     <>
       <div className="rounded-2xl border-1 border-[#D1D5DB] bg-white p-6">
         <div className="flex items-center justify-between">
-          <h2>Account Information</h2>
+          <h3 className="mb-6">Account Information</h3>
           <div>
             <LinkButton.Primary href="/password/change">Change</LinkButton.Primary>
           </div>
@@ -22,34 +23,26 @@ export const AccountInformation = ({
         <div>
           <ul className="list-none p-0">
             <li className="mb-4">
-              <div className="mb-1">
-                <strong>First name</strong>
-              </div>
+              <div className="mb-1 font-semibold">First name</div>
               <div>
                 <em>{firstName}</em>
               </div>
             </li>
             <li className="mb-4">
-              <div className="mb-1">
-                <strong>Last name</strong>
-              </div>
+              <div className="mb-1 font-semibold">Last name</div>
               <div>
                 <em>{lastName}</em>
               </div>
             </li>
             <li className="mb-4">
-              <div className="mb-1">
-                <strong>Email address:</strong>
-              </div>
+              <div className="mb-1 font-semibold">Email address</div>
               <div>
                 <em>{email}</em>
               </div>
             </li>
             <li className="">
-              <div className="mb-1">
-                <strong>Password</strong>
-              </div>
-              {/* Placeholder password characters used instead for security reasons */}
+              <div className="mb-1 font-semibold">Password</div>
+              {/* Placeholder password characters used instead of real password for security reasons */}
               <div>
                 &#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;
               </div>
--- app/account/page.tsx
+++ app/account/page.tsx
@@ -6,38 +6,39 @@ import { getServiceUrlFromHeaders } from "@lib/service-url";
 import { getSessionCredentials } from "@lib/cookies";
 import { loadSessionById } from "@lib/session";
 import { getUserByID } from "@lib/zitadel";
+import { Authentication } from "./components/Authentication";
+
+// TODO add translation strings
 
 export async function generateMetadata(): Promise<Metadata> {
   const { t } = await serverTranslation("otp");
-  return { title: t("verify.title") };
+  return { title: t("account.title") };
 }
 
 export default async function Page() {
   const _headers = await headers();
   const { serviceUrl } = getServiceUrlFromHeaders(_headers);
+
+  // Load account information from the session cookie
   const { sessionId, organization } = await getSessionCredentials();
-
-  const sessionData = await loadSessionById(serviceUrl, sessionId, organization);
-
-  const userId = sessionData.factors?.user?.id;
+  const session = await loadSessionById(serviceUrl, sessionId, organization);
+  const userId = session.factors?.user?.id;
   const userResponse = await getUserByID({ serviceUrl, userId: userId! });
-  const humanUser =
-    userResponse.user?.type.case === "human" ? userResponse.user?.type.value : undefined;
-
-  // const displayName = sessionData.factors?.user?.displayName;
-  // const loginName = sessionData.factors?.user?.loginName;
-  const email = humanUser?.email?.email;
-  const firstName = humanUser?.profile?.givenName;
-  const lastName = humanUser?.profile?.familyName;
+  const user = userResponse.user?.type.case === "human" ? userResponse.user?.type.value : undefined;
+  const firstName = user?.profile?.givenName;
+  const lastName = user?.profile?.familyName;
+  const email = user?.email?.email;
 
   if (!firstName || !lastName || !email) {
-    throw new Error("User information could not be retrieved");
+    throw new Error("User information could not be retrieved from session.");
   }
 
   return (
     <>
       <div>Account</div>
       <AccountInformation firstName={firstName} lastName={lastName} email={email} />
+      <div className="mb-10"></div>
+      <Authentication />
     </>
   );
 }
