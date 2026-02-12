df05a43a36afccc21c0497ebf06e2da82146e2cb
Peter Thiessen <peter.thiessen@cds-snc.ca>
2026-02-12 09:28:57 -0500

    revert removing reset page and update

---
+++ app/(ui)/password/reset/page.tsx
@@ -0,0 +1,56 @@
+import { Metadata } from "next";
+import { serverTranslation } from "@i18n/server";
+import { headers } from "next/headers";
+
+import { getPasswordComplexitySettings } from "@lib/zitadel";
+import { getServiceUrlFromHeaders } from "@lib/service-url";
+import { getSessionCredentials } from "@lib/cookies";
+import { loadSessionById } from "@lib/session";
+
+import { AuthPanel } from "@serverComponents/globals/AuthPanel";
+import { PasswordReset } from "./components/PasswordReset";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const { t } = await serverTranslation("password.set");
+  return { title: t("title") };
+}
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<Record<string | number | symbol, string | undefined>>;
+}) {
+  const _headers = await headers();
+  const { serviceUrl } = getServiceUrlFromHeaders(_headers);
+
+  const params = await searchParams;
+  const code = params.code;
+
+  const { sessionId, loginName, organization } = await getSessionCredentials();
+  const sessionFactors = await loadSessionById(serviceUrl, sessionId, organization);
+
+  const passwordComplexitySettings = await getPasswordComplexitySettings({
+    serviceUrl,
+    organization,
+  });
+
+  if (!loginName || !sessionId || !organization) {
+    throw new Error("No session.");
+  }
+
+  return (
+    <AuthPanel
+      titleI18nKey="password.title"
+      descriptionI18nKey="password.description"
+      namespace="reset"
+    >
+      <PasswordReset
+        userId={sessionFactors.factors?.user?.id}
+        passwordComplexitySettings={passwordComplexitySettings}
+        code={code}
+        organization={organization}
+        loginName={loginName}
+      />
+    </AuthPanel>
+  );
+}
