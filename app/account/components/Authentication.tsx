6e45aff62d1da948b8ef26a5aafcb4212e487f44
Peter Thiessen <peter.thiessen@cds-snc.ca>
2026-02-18 11:32:20 -0500

    clean up logic

--- app/account/components/Authentication.tsx
+++ app/account/components/Authentication.tsx
@@ -19,6 +19,7 @@ export const Authentication = ({
   authenticatorStatus: boolean;
 }) => {
   const { t } = useTranslation("account");
+  const hasMFAMethods = (Array.isArray(u2fList) && u2fList.length > 0) || authenticatorStatus;
 
   const handleRemoveU2F = async (u2fId: string) => {
     const result = await removeU2FAction(userId, u2fId);
@@ -49,62 +50,69 @@ export const Authentication = ({
       <div className="rounded-2xl border-1 border-[#D1D5DB] bg-white p-6">
         <h3 className="mb-6">{t("authentication.title")}</h3>
 
-        {u2fList.length === 0 && !authenticatorStatus && <p>{t("authentication.noTwoFactor")}</p>}
+        {!hasMFAMethods && <p>{t("authentication.noTwoFactor")}</p>}
 
-        <div>
-          <ul className="list-none p-0">
-            {u2fList.length > 0 &&
-              u2fList
-                .sort((a, b) => (a.name || "").localeCompare(b.name || ""))
-                .map((data) => {
-                  return (
-                    <li key={data.id} className="mb-4">
-                      <Image
-                        src={getImageUrl("/img/fingerprint_24px.png")}
-                        alt=""
-                        width={32}
-                        height={32}
-                        className="mr-2 inline-block"
-                      />
-                      <span className="mr-2 font-semibold">{t("authentication.securityKey")}</span>
-                      <span>({data.name || t("authentication.unknownDevice")})</span>
-                      <span className="mx-2">&#8226;</span>
-                      <Button onClick={() => handleRemoveU2F(data.id)} theme="link">
-                        {t("authentication.remove")}
-                      </Button>
-                    </li>
-                  );
-                })}
+        {hasMFAMethods && (
+          <>
+            <div>
+              <ul className="list-none p-0">
+                {u2fList.length > 0 &&
+                  u2fList
+                    .sort((a, b) => (a.name || "").localeCompare(b.name || ""))
+                    .map((data) => {
+                      return (
+                        <li key={data.id} className="mb-4">
+                          <Image
+                            src={getImageUrl("/img/fingerprint_24px.png")}
+                            alt=""
+                            width={32}
+                            height={32}
+                            className="mr-2 inline-block"
+                          />
+                          <span className="mr-2 font-semibold">
+                            {t("authentication.securityKey")}
+                          </span>
+                          <span>({data.name || t("authentication.unknownDevice")})</span>
+                          <span className="mx-2">&#8226;</span>
+                          <Button onClick={() => handleRemoveU2F(data.id)} theme="link">
+                            {t("authentication.remove")}
+                          </Button>
+                        </li>
+                      );
+                    })}
 
-            {authenticatorStatus && (
-              <li className="mb-4">
-                <Image
-                  src={getImageUrl("/img/verified_user_24px.png")}
-                  alt=""
-                  width={32}
-                  height={32}
-                  className="mr-2 inline-block"
-                />
-                <span className="mr-2 font-semibold">{t("authentication.authenticatorApp")}</span>
-                <span className="mx-2">&#8226;</span>
-                <Button onClick={handleRemoveAuthenticator} theme="link">
-                  {t("authentication.remove")}
-                </Button>
-              </li>
-            )}
-          </ul>
-        </div>
-
-        <div className="mt-6 flex align-middle">
-          <Image
-            src={getImageUrl("/img/plus.svg")}
-            alt=""
-            width={24}
-            height={24}
-            className="mr-1"
-          />{" "}
-          <Link href="/mfa/set">{t("authentication.addlMethods")}</Link>
-        </div>
+                {authenticatorStatus && (
+                  <li className="mb-4">
+                    <Image
+                      src={getImageUrl("/img/verified_user_24px.png")}
+                      alt=""
+                      width={32}
+                      height={32}
+                      className="mr-2 inline-block"
+                    />
+                    <span className="mr-2 font-semibold">
+                      {t("authentication.authenticatorApp")}
+                    </span>
+                    <span className="mx-2">&#8226;</span>
+                    <Button onClick={handleRemoveAuthenticator} theme="link">
+                      {t("authentication.remove")}
+                    </Button>
+                  </li>
+                )}
+              </ul>
+            </div>
+            <div className="mt-6 flex align-middle">
+              <Image
+                src={getImageUrl("/img/plus.svg")}
+                alt=""
+                width={24}
+                height={24}
+                className="mr-1"
+              />{" "}
+              <Link href="/mfa/set">{t("authentication.addlMethods")}</Link>
+            </div>
+          </>
+        )}
       </div>
       <ToastContainer autoClose={false} containerId="account-authentication" />
     </>
