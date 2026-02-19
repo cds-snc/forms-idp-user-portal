f37d006521910fd299aa8a11ccee4de0a7d908a7
Peter Thiessen <peter.thiessen@cds-snc.ca>
2026-02-19 15:03:02 -0500

    update new account register link to remove existing session

--- app/account/components/VerifiedAccount.tsx
+++ app/account/components/VerifiedAccount.tsx
@@ -28,7 +28,7 @@ export const VerifiedAccount = ({ email }: { email: string }) => {
               ns="account"
               components={[
                 <strong key="0" />,
-                <Link key="1" href="/register" />,
+                <Link key="1" href="/logout-session?returnUrl=/register" />,
                 <Link key="2" href={`${FORMS_PRODUCTION_URL}/${language}/support`} />,
               ]}
             />
