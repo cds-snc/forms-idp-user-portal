a8fcce0f0da74f3414c266b703a274579a636917
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2026-02-19 14:06:58 -0500

    Get org from consts

--- app/(auth)/password/reset/page.tsx
+++ app/(auth)/password/reset/page.tsx
@@ -3,10 +3,9 @@ import { getPasswordComplexitySettings } from "@lib/zitadel";
 import { Metadata } from "next";
 import { serverTranslation } from "@i18n/server";
 import { headers } from "next/headers";
-import { getSessionCredentials } from "@lib/cookies";
-
 import { AuthPanel } from "@serverComponents/globals/AuthPanel";
 import { PasswordResetFlow } from "./components/PasswordResetFlow";
+import { ZITADEL_ORGANIZATION } from "@root/constants/config";
 
 export async function generateMetadata(): Promise<Metadata> {
   const { t } = await serverTranslation("password");
@@ -14,7 +13,7 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default async function Page() {
-  const { organization } = await getSessionCredentials();
+  const organization = ZITADEL_ORGANIZATION;
   const _headers = await headers();
   const { serviceUrl } = getServiceUrlFromHeaders(_headers);
 
