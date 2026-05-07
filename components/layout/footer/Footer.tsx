e26df88db5b5cce0ecaa17bac6a37df5972cd22a
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2026-05-07 07:22:55 -0400

    update components to not prop drill language property

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -30,3 +30,23 @@ export const Footer = async ({ children }: { children?: React.ReactNode }) => {
     </footer>
   );
 };
+
+export const FooterSkeleton = () => (
+  <footer
+    className={cn(
+      "mt-16 flex-none border-0 bg-gray-100 px-4 py-0 tablet:px-16 laptop:px-32 lg:mt-10"
+    )}
+    data-server="true"
+    data-testid="footer"
+  >
+    <div className="flex flex-row items-center justify-between pt-10 pb-5 lg:flex-col lg:items-start lg:gap-4">
+      <div></div>
+
+      <div className="min-w-42">
+        <picture>
+          <img className="h-10 lg:h-8" alt="fip.text" src={getImageUrl("/img/wmms-blk.svg")} />
+        </picture>
+      </div>
+    </div>
+  </footer>
+);
